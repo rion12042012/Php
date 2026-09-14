e5eec12affaac3761a6136929ec70887aea71ef8
rion12042012 <rion.dermaku2012@gmail.com>
2026-09-14 16:05:20 +0200

    hi

--- Module3/index.php
+++ Module3/index.php
@@ -48,7 +48,58 @@ if($number < 0){
     echo "The value of $number is a positive number";
 }
 
+//Loops
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
 
 
+$x = 1;
+while($x <= 5){
+    echo "The number is: $x <br>";
+    $x++;
+}
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+ 
+ $y = 1;
+ do{
+    echo "The number is: $y <br>";
+    $x++;
+ }while($y >= 5);
+    
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
 
+ for($z=9; $z <=16; $z++){
+    echo "The number is;$z <br>";
+ }
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+$cars = array("BMW","VW","BYC","RAM","AUDI","FORD","FERRARI");
+foreach($cars as $car ){
+    echo "$car <br>";
+}
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+  
+$age = array("John" => 18, "Michael" => 20, "Joe" => 13);
+ foreach($age as $key => $value){
+    echo "$key = $value  <br>";
+ }
+      
 ?>
